e63c2b41ec8df4d757635d61282f6bde1db59f71
Jimin Kim <jmkim000000@gmail.com>
2024-01-19 13:05:34 +0900

    refactor: 댓글 css 적용 방식 변경

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -9,23 +9,24 @@ export default function BlogPostItemWrapper(props) {
   return (
     <>
       <BlogPostItem {...props} />
-      {isBlogPostPage && (
-        <Giscus
-          id='blog-comments'
-          repo="jmkim0/blog"
-          repoId="R_kgDOK-1fHg"
-          category="giscus"
-          categoryId="DIC_kwDOK-1fHs4CcgWw"
-          mapping="pathname"
-          strict="0"
-          reactionsEnabled="1"
-          emit-metadata="0"
-          inputPosition="bottom"
-          theme="preferred_color_scheme"
-          lang="ko"
-          loading="lazy"
-        />
-      )}
+      <div className='margin-top--lg'>
+        {isBlogPostPage && (
+          <Giscus
+            repo="jmkim0/blog"
+            repoId="R_kgDOK-1fHg"
+            category="giscus"
+            categoryId="DIC_kwDOK-1fHs4CcgWw"
+            mapping="pathname"
+            strict="0"
+            reactionsEnabled="1"
+            emit-metadata="0"
+            inputPosition="bottom"
+            theme="preferred_color_scheme"
+            lang="ko"
+            loading="lazy"
+          />
+        )}
+      </div>
     </>
   );
 }
--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -1,27 +1,30 @@
 import React from 'react';
+import clsx from 'clsx';
 import DocItem from '@theme-original/DocItem';
 import Giscus from '@giscus/react';
+
+import styles from '@docusaurus/theme-classic/src/theme/DocItem/Layout/styles.module.css';
 
 export default function DocItemWrapper(props) {
   return (
     <>
       <DocItem {...props} />
-      <Giscus
-        id='doc-comments'
-        className="docItemCol"
-        repo="jmkim0/blog"
-        repoId="R_kgDOK-1fHg"
-        category="giscus"
-        categoryId="DIC_kwDOK-1fHs4CcgWw"
-        mapping="pathname"
-        strict="0"
-        reactionsEnabled="1"
-        emit-metadata="0"
-        inputPosition="bottom"
-        theme="preferred_color_scheme"
-        lang="ko"
-        loading="lazy"
-      />
+      <div className={clsx('margin-top--lg', styles.docItemCol)}>
+        <Giscus
+          repo="jmkim0/blog"
+          repoId="R_kgDOK-1fHg"
+          category="giscus"
+          categoryId="DIC_kwDOK-1fHs4CcgWw"
+          mapping="pathname"
+          strict="0"
+          reactionsEnabled="1"
+          emit-metadata="0"
+          inputPosition="bottom"
+          theme="preferred_color_scheme"
+          lang="ko"
+          loading="lazy"
+        />
+      </div>
     </>
   );
 }
